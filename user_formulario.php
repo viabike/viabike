fa9513863a2d9e76b9aa27b5b0dd51ac99b63c15
Lucas Marques <lukkeemarxs@gmail.com>
2015-10-20 17:11:54 -0200

    Interface do login e cadastro

--- user_formulario.php
+++ user_formulario.php
@@ -1,6 +1,19 @@
-<form class="" action="user_confirmaCad.php" method="post">
-	nome<input type="text" name="nome" value=""><br>
-	apelido<input type="text" name="username" value=""><br>
-	senha<input type="password" name="senha" value=""><br>
-	<input type="submit" name="" value="Cadastrar">
-</form>
+<?php include("template/header.php");?>
+	<CENTER>
+	<div id='form_user'>
+		<form action="confirma_ponto.php" class="form_login" method="POST">
+			<h1>Cadastre-se</h1><br>
+			Nome Completo:<input type="text" name="nome" class="form"><br>
+			Apelido:<input type="text" name="apelido" class="form"><br>
+			Senha:<input type="text" name="email" class="form"><br>
+			<input type="submit" value="Cadastrar" class="button">
+		</form>
+		<form action="confirma_ponto.php" class="form_cadastro" method="POST">
+			<h1>Entrar</h1><br>
+			Apelido:<input type="text" name="nome_usuario" class="form"><br>
+			Senha:<input type="password" name="senha" class="form"><br>
+			<input type="submit" value="Entrar" class="button">
+		</form>
+	</div>
+	</CENTER>
+<?php include("template/footer.php");?>
