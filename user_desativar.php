0b21ed1c46ff139864dab39ee1386a18674a0af7
itallo ferreira <itallorsf@gmail.com>
2015-10-27 17:24:01 -0200

    Modificações na opção de desativar a conta (não terminado)

--- user_desativar.php
+++ user_desativar.php
@@ -5,9 +5,8 @@ $conexao = conectar();
 
 $id_usuario = $_POST['id_usuario'];
 
-//========================= QUERY DELETE =========================
-$user_desativa = $conexao -> prepare("DELETE FROM usuario where id_usuario = :id_usuario");
-  $user_desativa -> bindValue(":id_usuario" , $id_usuario, PDO::PARAM_INT);
+$user_desativa = $conexao -> prepare("UPDATE `viabike_db`.`usuario` SET `usuario_ativo` = false WHERE `usuario`.`id_usuario` = $id_usuario;");
+$user_desativa -> bindValue(":id_usuario" , $id_usuario, PDO::PARAM_INT);
 $user_desativa -> execute();
 
 header("location:user_painel.php"); die();
