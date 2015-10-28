e89f115417c6afa4a5ea2bc5ebd35d6e3a9d4999
itallo ferreira <itallorsf@gmail.com>
2015-10-28 00:11:52 -0200

    Adicionado links para as redes sociais de todos da equipe

--- equipe.php
+++ equipe.php
@@ -20,29 +20,53 @@ $(document).ready(function(){
 <div class="desenvolvedor">
 	<center><img src="imagens/equipe/renan-pb.png" width="150px" alt="Renan Cavichi" class="img-pb">
 	<img src="imagens/equipe/renan.png" width="150px" alt="Renan Cavichi" class="img-hover"></center>
-	<center><h3>Renan Cavichi</h3>
-	<p><i class="fa fa-linkedin-square in"></i> <i class="fa fa-envelope-square mail"></i> <i class="fa fa-facebook-square face"></i></p></center>
+	<center>
+		<h3>Renan Cavichi</h3>
+		<p>
+			<a href="https://www.linkedin.com/pub/renan-cavichi/3b/b33/311"><i class="fa fa-linkedin-square in"></i></a>
+			<a href="mailto:renancavichi@gmail.com"><i class="fa fa-envelope-square mail"></i></a>
+			<a href="https://www.facebook.com/renancavichi"><i class="fa fa-facebook-square face"></i></a>
+		</p>
+	</center>
 </div>
 
 <div class="desenvolvedor">
 	<center><img src="imagens/equipe/brian-pb.png" width="150px" alt="Brian Soares" class="img-pb">
 	<img src="imagens/equipe/brian.png" width="150px" alt="Brian Soares" class="img-hover"></center>
-	<center><h3>Brian Soares</h3>
-	<p><i class="fa fa-linkedin-square in"></i> <i class="fa fa-envelope-square mail"></i> <i class="fa fa-facebook-square face"></i></p></center>
+	<center>
+		<h3>Brian Soares</h3>
+		<p>
+			<a href="https://www.linkedin.com/pub/brian-soares/105/388/569"><i class="fa fa-linkedin-square in"></i></a>
+			<a href="mailto:brian_caragua@hotmail.com"><i class="fa fa-envelope-square mail"></i></a>
+			<a href="https://www.facebook.com/Br.Soa.Nasc.Santos"><i class="fa fa-facebook-square face"></i></a>
+		</p>
+	</center>
 </div>
 
 <div class="desenvolvedor">
 	<center><img src="imagens/equipe/haryel-pb.png" width="150px" alt="Haryel Luyde" class="img-pb">
 	<img src="imagens/equipe/haryel.png" width="150px" alt="Haryel Luyde" class="img-hover"></center>
-	<center><h3>Haryel Luyde</h3>
-	<p><i class="fa fa-linkedin-square in"></i> <i class="fa fa-envelope-square mail"></i> <i class="fa fa-facebook-square face"></i></p></center>
+	<center>
+		<h3>Haryel Luyde</h3>
+		<p>
+			<a href="https://www.linkedin.com/pub/haryel-luyde/104/48b/425"><i class="fa fa-linkedin-square in"></i></a>
+			<a href="mailto:haryelllc@gmail.com"><i class="fa fa-envelope-square mail"></i></a>
+			<a href="https://www.facebook.com/haryel.luyde"><i class="fa fa-facebook-square face"></i></a>
+		</p>
+	</center>
 </div>
 
 <div class="desenvolvedor margin_des">
 	<center><img src="imagens/equipe/itallo-pb.png" width="150px" alt="Itallo Ferreira" class="img-pb">
 	<img src="imagens/equipe/itallo.png" width="150px" alt="Itallo Ferreira" class="img-hover"></center>
-	<center><h3>Itallo Ferreira</h3>
-	<p><i class="fa fa-linkedin-square in"></i> <i class="fa fa-envelope-square mail"></i> <i class="fa fa-facebook-square face"></i></p></center>
+	<center>
+		<h3>Itallo Ferreira</h3>
+		<p>
+			<a href="https://www.linkedin.com/in/italloferreira"><i class="fa fa-linkedin-square in"></i></a>
+			<a href="mailto:itallorsf@gmail.com"><i class="fa fa-envelope-square mail"></i></a>
+			<a href="https://www.facebook.com/italloferreira"><i class="fa fa-facebook-square face"></i></a>
+		</p>
+	</center>
 </div>
 
 <div class="esp"></div>
@@ -50,28 +74,52 @@ $(document).ready(function(){
 <div class="desenvolvedor">
 	<center><img src="imagens/equipe/jorge-pb.png" width="150px" alt="Jorge Fernando" class="img-pb">
 	<img src="imagens/equipe/jorge.png" width="150px" alt="Jorge Fernando" class="img-hover"></center>
-	<center><h3>Jorge Fernando</h3>
-	<p><i class="fa fa-linkedin-square in"></i> <i class="fa fa-envelope-square mail"></i> <i class="fa fa-facebook-square face"></i></p></center>
+	<center>
+		<h3>Jorge Fernando</h3>
+		<p>
+			<a href="https://www.linkedin.com/pub/jorge-fernandes/108/876/a1a"><i class="fa fa-linkedin-square in"></i></a>
+			<a href="mailto:jorge-fernando-santos16@hotmail.com"><i class="fa fa-envelope-square mail"></i></a>
+			<a href="https://www.facebook.com/jorge.fernando.98892615"><i class="fa fa-facebook-square face"></i></a>
+		</p>
+	</center>
 </div>
 
 <div class="desenvolvedor">
 	<center><img src="imagens/equipe/lucas-pb.png" width="150px" alt="Lucas Marques" class="img-pb">
 	<img src="imagens/equipe/lucas.png" width="150px" alt="Lucas Marques" class="img-hover"></center>
-	<center><h3>Lucas Marques</h3>
-	<p><i class="fa fa-linkedin-square in"></i> <i class="fa fa-envelope-square mail"></i> <i class="fa fa-facebook-square face"></i></p></center>
+	<center>
+		<h3>Lucas Marques</h3>
+		<p>
+			<a href="https://br.linkedin.com/pub/lucas-marques/104/685/416"><i class="fa fa-linkedin-square in"></i></a>
+			<a href="mailto:lukkeemarques@gmail.com"><i class="fa fa-envelope-square mail"></i></a>
+			<a href="https://www.facebook.com/lucas.marquesdeoliveira.758"><i class="fa fa-facebook-square face"></i></a>
+		</p>
+	</center>
 </div>
 
 <div class="desenvolvedor">
 	<center><img src="imagens/equipe/perrota-pb.png" width="150px" alt="Lucas Perrotta" class="img-pb">
 	<img src="imagens/equipe/perrota.png" width="150px" alt="Lucas Perrotta" class="img-hover"></center>
-	<center><h3>Lucas Perrotta</h3>
-	<p><i class="fa fa-linkedin-square in"></i> <i class="fa fa-envelope-square mail"></i> <i class="fa fa-facebook-square face"></i></p></center>
+	<center>
+		<h3>Lucas Perrotta</h3>
+		<p>
+			<a href="https://www.linkedin.com/pub/lucas-perrotta-barbosa/104/abb/b54"><i class="fa fa-linkedin-square in"></i></a>
+			<a href="mailto:perrotta.lucas@gmail.com"><i class="fa fa-envelope-square mail"></i></a>
+			<a href="https://www.facebook.com/lucas.perrottabarbosa"><i class="fa fa-facebook-square face"></i></a>
+		</p>
+	</center>
 </div>
 
 <div class="desenvolvedor margin_des">
 	<center><img src="imagens/equipe/william-pb.png" width="150px" alt="William de Melo" class="img-pb">
 	<img src="imagens/equipe/william.png" width="150px" alt="William de Melo" class="img-hover"></center>
-	<center><h3>William de Melo</h3>
-	<p><i class="fa fa-linkedin-square in"></i> <i class="fa fa-envelope-square mail"></i> <i class="fa fa-facebook-square face"></i></p></center>
+	<center>
+		<h3>William de Melo</h3>
+		<p>
+			<a href="https://www.linkedin.com/pub/william-melo/a2/215/173"><i class="fa fa-linkedin-square in"></i></a>
+			<a href="mailto:willblood33@gmail.com"><i class="fa fa-envelope-square mail"></i></a>
+			<a href="https://www.facebook.com/william.melo.7165"><i class="fa fa-facebook-square face"></i></a>
+		</p>
+	</center>
 </div>
 <?php include("template/footer.php");?>
