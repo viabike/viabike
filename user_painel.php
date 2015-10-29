23fcc2bf5b839810943cd864128da63224587bc0
Haryel Luyde <haryelllc@gmail.com>
2015-10-29 16:28:41 -0200

    Interface do usuário

--- user_painel.php
+++ user_painel.php
@@ -2,6 +2,9 @@
 include("conexao/conexao.php");
 include("template/header.php");
 
+echo '<h1 style="float: left; text-align: left">Editar Perfil</h1>
+<a href="#"><button class="button" style="float: right;">Alterar senha</button></a></h1><br>';
+
 $conexao = conectar();//Conexao com o banco de dados viabike_db
 
 $user_buscador = $conexao -> prepare("SELECT * FROM usuario WHERE email = '".$_COOKIE['email']."'");//pegando todos os usuarios cadastrados
@@ -11,25 +14,20 @@ $user_buscador -> execute();//executando a query de uma maneira segura
 $user = $user_buscador->fetchAll(PDO::FETCH_OBJ);?>
 
 <?php foreach ($user as $usuario): ?>
-			<!-- 
+			<!--
 				FALTA AINDA UMA CONFIRMAÇÃO PARA NAO DESATIVAR POR ENGANO
 			-->
-			Nome: <?=$usuario->nome?>
-			<br>
-			Email: <?=$usuario->email?>
-		
-			<form action="user_desativar.php" method="post">
+
+			<form action="user_altera.php" method="POST">
 				<input type="hidden" name="id_usuario" value="<?=$usuario->id_usuario?>">
-				<input class="button" type="submit" value="Desativar Conta">
+				Nome:<input type="text" name="nome" class="input" value="<?=$usuario->nome?>" placeholder="Ex: Exemplo de Nome" required><br>
+				E-mail:<input type="email" name="email" class="input" value="<?=$usuario->email?>" placeholder="Ex: ex@exemplo.com" required><br>
+				<hr style="border:1px; padding:2px; margin-bottom:10px; border-radius:5px; background-color:rgba(204,204,204, 0.25);">
+				Senha:<input type="password" name="senha" class="input" required><br>
+				<a href="user_desativar.php" style="float: left;font-size:14px; line-height:65px; color:#535455;">Dasativar conta</button></a>
+				<input type="submit" value="Alterar" class="button" style="float: right">
 			</form>
-			
-			<br>
-			
-			<form action="user_altera_form.php" method="post">
-				<input type="hidden" name="id_usuario" value="<?=$usuario->id_usuario?>">
-				<input class="button" type="submit" value="Alterar">
-			</form>
-		
+
 <?php endforeach;
 include("template/footer.php");
 ?>
