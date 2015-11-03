5453eaa2094a0dcf398a276d19ede5a78fe10f55
itallo ferreira <itallorsf@gmail.com>
2015-11-03 17:23:42 -0200

    Adicionado verificação se o usuário já utilizou o email anteriormente

--- user_confirmaCad.php
+++ user_confirmaCad.php
@@ -1,20 +1,52 @@
+<body onload=iniciarContagem()>
+	<script>
+	// SCRIPT PARA REDIRECIONAR PARA OUTRA PÁGINA DEPOIS DE UM DETERMINADO TEMPO
+		//tempo para o redirecionamento em segundos
+		var cont = 10;
+
+		function iniciarContagem()
+		{
+		   if ((cont - 1) >= 0)
+		   {
+			   cont--;
+			   contagem.innerText = 'Você será redirecionado para a página inicial em ' + cont + ' segundos';
+			   setTimeout('iniciarContagem()',1000);
+		   }
+		   if (cont == 0)
+		   {
+				window.location.href='index.php';
+		   }
+		}
+	</script>
+
 <?php
 include_once("conexao/conexao.php");
+include_once("template/header.php");
 
 $conexao = conectar();//Funcão de conexão com o banco de dados viabike_db
 
-$nome      = addslashes(trim($_POST["nome"   ]));//requisição vinda do arquivo user_formulario.php
-$email   = addslashes(trim($_POST["email"]));//requisição vinda do arquivo user_formulario.php
-$senha     = sha1(strtolower($_POST['senha'  ]));//requisição vinda do arquivo user_formulario.php
+$nome		= addslashes(trim($_POST["nome"]));//requisição vinda do arquivo user_formulario.php
+$email	= addslashes(trim($_POST["email"]));//requisição vinda do arquivo user_formulario.php
+$senha	= sha1(strtolower($_POST['senha']));//requisição vinda do arquivo user_formulario.php
 
 $user_verif = $conexao->prepare("SELECT * FROM usuario WHERE email = :email");
 $user_verif -> bindValue(":email", $email);
 $user_verif -> execute();
-$num_rows = $user_verif->fetchColumn();
-
-if($num_rows == 1)
+while ($linha = $user_verif->fetch(PDO::FETCH_ASSOC))
 {
-	echo "Usuário já cadastrado!";
+	$Bemail = $linha['email'];
+}
+
+if($Bemail == $email)
+{
+	echo "
+		<div>
+			Este email já foi utilizado anteriormente, possivelmente a conta foi desativada. <br> Faça login pra ativar novamente esta conta. <br> <a href='user_formulario.php'>Clique para ir para login.</a>
+			<br><br>
+			<b><div id=contagem></div></b>
+			<a href='index.php'>Clique para ir para a página incial agora.</a>
+		</div>
+	";
 }
 else
 {
@@ -29,9 +61,5 @@ else
   header("location: user_sucesso_cadastro.php");
 }
 
-
-
-//========================================
-
-
+include_once("template/footer.php");
 ?>
