d62e7b40b9f17c0d8ea0bcadc398d4a31bd1b1b0
William de Melo <williamhexadc@gmail.com>
2015-11-10 16:31:12 -0200

    ADD campo data_public e reorganização do código

--- sinal_confirmaCad.php
+++ sinal_confirmaCad.php
@@ -1,23 +1,25 @@
 <?php
 require_once("conexao/conexao.php");
+$conexao = conectar();
+//REQUISIÇÕES VINDAS DA PÁGINA SINAL_FORMULARIO.PHP
+$titulo      = addslashes(trim($_POST['titulo'     ]));
+$categoria   = addslashes(trim($_POST['categoria'  ]));
+$descricao   = addslashes(trim($_POST['descricao'  ]));
+$longitude   = addslashes(trim($_POST['longitude'  ]));
+$latitude    = addslashes(trim($_POST['latitude'   ]));
+$data_public = addslashes(trim($_POST['data_public']));
 
-$titulo    = addslashes(trim($_POST['titulo']   ));
-$categoria = addslashes(trim($_POST['categoria']));
-$descricao = addslashes(trim($_POST['descricao']));
-$longitude = addslashes(trim($_POST['longitude']));
-$latitude  = addslashes(trim($_POST['latitude'] ));
-
-$addsinal = $conexao -> prepare("INSERT sinalizacao (titulo, latitude, longitude, categoria, descricao) values(:titulo, :latitude, :longitude, :categoria, :descricao)");
-$addsinal->bindValue(":titulo"    , $titulo   );
-$addsinal->bindValue(":latitude"  , $latitude );
-$addsinal->bindValue(":longitude" , $longitude);
-$addsinal->bindValue(":categoria" , $categoria);
-$addsinal->bindValue(":descricao" , $descricao);
+$addsinal = $conexao -> prepare("INSERT INTO sinalizacao (titulo, descricao, latitude, longitude, categoria, data_public) values(:titulo, :descricao, :latitude, :longitude, :categoria, :data_public)");
+$addsinal->bindValue(":titulo"      , $titulo      );
+$addsinal->bindValue(":descricao"   , $descricao   );
+$addsinal->bindValue(":latitude"    , $latitude    );
+$addsinal->bindValue(":longitude"   , $longitude   );
+$addsinal->bindValue(":categoria"   , $categoria   );
+$addsinal->bindValue(":data_public" , $data_public );
 $addsinal->execute();
 
 if($addsinal) {
   echo "sinal add com sucesso";
-  echo var_dump($addsinal);
 }else {
   echo "faio";
 }
