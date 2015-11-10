8e654155bc84d5e6d6681d8c2063b441fa870946
William de Melo <williamhexadc@gmail.com>
2015-11-10 17:15:56 -0200

    Inclusão do form de alterar numa tabela

--- sinal_exibir.php
+++ sinal_exibir.php
@@ -13,6 +13,7 @@ $exibir = $exibir_sinal->fetchAll(PDO::FETCH_OBJ);?>
     <td>Categoria</td>
     <td>Descricao</td>
     <td>Data de Publicação</td>
+    <td>Alterar</td>
   </tr>
   <?php foreach ($exibir as $sinal): ?>
   <tr>
@@ -20,6 +21,10 @@ $exibir = $exibir_sinal->fetchAll(PDO::FETCH_OBJ);?>
     <td><?=$sinal->categoria?></td>
     <td><?=$sinal->descricao?></td>
     <td><?=$sinal->data_public?></td>
+    <td><form action="sinal_altera_form.php" method="post">
+      <input type="hidden" name="id_sinal" value="<?=$sinal->id_sinal?>">
+      <input type="submit" value="alterar">
+    </form></td>
   </tr>
   <?php endforeach; ?>
 </table>
