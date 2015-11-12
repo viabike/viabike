2b84e5d43adfe984c264b2b36bd12aef321e16a6
William de Melo <williamhexadc@gmail.com>
2015-11-12 14:24:43 -0200

    Formulario de alteração de sinais já em completo funcionamento

--- sinal_altera_form.php
+++ sinal_altera_form.php
@@ -3,11 +3,10 @@ require_once("conexao/conexao.php");
 
 $conexao = conectar();
 
-$id_sinal = $_POST['id_sinal']; //Requisição vinda da pag sinal_exibir...OBS: mudar depois, definir nome
 
 //preparando a query
 $sinal_buscador = $conexao -> prepare("SELECT * from sinalizacao where id_sinal = :id_sinal");
-    $sinal_buscador -> bindValue(":id_sinal" , $id_sinal);
+$sinal_buscador -> bindValue(":id_sinal" , $_POST['id_sinal']);
 $sinal_buscador -> execute();
 //executando a query
 
@@ -19,12 +18,12 @@ foreach ($sinal as $sinalizacao) :?>
     <input type="hidden" name="id_sinal" value="<?=$sinalizacao->id_sinal?>">
     Titulo:<input type="text" name="titulo" value="<?=$sinalizacao->titulo?>"><br>
     Categoria:<select name="categoria">
-    <option value="OB" selected>Obras</option>
-    <option value="IT">Interditado</option>
-    <option value="AC">Acidentado</option>
-    <option value="OT">Outros</option>
+    <option value="OB" <?=($sinalizacao->categoria == "OB")?'selected':''?>>Obras</option>
+    <option value="IT" <?=($sinalizacao->categoria == "IT")?'selected':''?>>Interditado</option>
+    <option value="AC" <?=($sinalizacao->categoria == "AC")?'selected':''?>>Acidentado</option>
+    <option value="OT" <?=($sinalizacao->categoria == "OT")?'selected':''?>>Outros</option>
     </select><br>
-    Descrição:<textarea name="descricao" value="<?=$sinalizacao->descricao?>">
+    Descrição:<textarea name="descricao">
     </textarea><br>
     Longitude<input type="number" name="longitude" value="<?=$sinalizacao->longitude?>"><br>
     Latitude:<input type="number" name="latitude" value="<?=$sinalizacao->latitude?>"><br>
