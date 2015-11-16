27ac6af516a4c19b6113553fabb9aa6ac52adccf
Itallo Ferreira <itallorsf@gmail.com>
2015-11-16 17:49:40 -0200

    Busca dos pontos de interesse no banco funcionando corretamente

--- filtro_pega_ponto.php
+++ filtro_pega_ponto.php
@@ -1,18 +1,21 @@
 <?php
 
-// NAO TERMINADO
 require_once("conexao/conexao.php");
 $conexao = conectar();
 
 
-$filtro_ponto = $_POST['filtro_ponto'];
+$filtro_ponto = $_GET['filtro_ponto'];
 
-$consulta = $conexao->prepare("SELECT * FROM ponto_interesse WHERE categoria = :filtro_ponto");
-    $consulta->bindValue(":filtro_ponto" , $filtro_ponto);
-    $consulta->execute();
 
-$linha = $consulta->fetch(PDO::FETCH_OBJ);
-
-foreach ($linhe as $linhas){
-    echo json_encode($linhas);
+if ($filtro_ponto == "TODOS") {
+	$consulta = $conexao->query("SELECT * FROM ponto_interesse");
 }
+else {
+	$consulta = $conexao->query("SELECT * FROM ponto_interesse WHERE categoria = '".$filtro_ponto."'");
+}
+
+$linha = $consulta->fetchAll(PDO::FETCH_OBJ);
+
+
+echo json_encode($linha);
+
