585da0816f2e7a54625480464ef9d10cd29f6b70
Itallo Ferreira <itallorsf@gmail.com>
2015-11-17 16:01:52 -0200

    Adicionado VIEWPORT

--- m/template/header.php
+++ m/template/header.php
@@ -7,6 +7,7 @@ include_once("funcoes/funcoes.php");
     <head>
         <title>Viabike.me</title>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
         <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
