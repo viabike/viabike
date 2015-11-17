9ec2e7d3aa4d4c6ec4a3c676bcc35ca2a6c279c6
Itallo Ferreira <itallorsf@gmail.com>
2015-11-17 16:25:49 -0200

    Adicionado script do botão de filtros

--- m/js/script.js
+++ m/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	$("#menu").hide();
+	// Menu de navegação
+        $("#menu").hide();
 	var aux = 0;
 	
 	$("#nav" ).click(function() {
@@ -11,5 +12,22 @@ $(document).ready(function(){
 			$("#menu").hide();
 			aux = 0;
 		}
-	});	
+	});
+        
+        // Filtros
+        $("#filtros-menu").hide();
+	var aux = 0;
+	
+	$("#filtros" ).click(function() {
+		if (aux == 0) {
+			aux = 1;
+			$("#filtros-menu" ).show();
+		}
+		else {
+			$("#filtros-menu").hide();
+			aux = 0;
+		}
+	});
+        
+        
 });
