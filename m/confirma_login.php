bca913967631b3b065bca206d6ca86ec7739af33
itallo ferreira <itallorsf@gmail.com>
2015-11-21 13:58:03 -0200

    Correção no comportamento ao der erro no login

--- m/confirma_login.php
+++ m/confirma_login.php
@@ -1,44 +1,61 @@
 <?php
+
 session_start();
 require_once("../conexao/conexao.php");
 
 // dados vindos da página index
-$user	=	$_POST['email'];
-$senha	=	sha1(strtolower($_POST['senha']));
+$user = $_POST['email'];
+$senha = sha1(strtolower($_POST['senha']));
 
 $conexao = conectar();
 
 //cria as vários com dados do banco
-$nomeb;
-$userb;
-$senhab;
-$tipob;
+$nomeb = null;
+$userb = null;
+$senhab = null;
+$tipob = null;
+$idb = null;
 
 //pega os dados do banco
-$consulta = $conexao->query("SELECT nome, email, senha, tipo_usuario FROM usuario where email = '".$user."'");
-while	($linha = $consulta->fetch(PDO::FETCH_OBJ)){
-	// dados vindos do banco, por isso o "b"
-	$nomeb	= $linha->nome;
-	$userb	= $linha->email;
-	$senhab	= $linha->senha;
-	$tipob	= $linha->tipo_usuario;
+$consulta = $conexao->query("SELECT id_usuario, nome, email, senha, tipo_usuario FROM usuario where email = '" . $user . "'");
+while ($linha = $consulta->fetch(PDO::FETCH_OBJ)) {
+    // dados vindos do banco, por isso o "b"
+    $idb = $linha->id_usuario;
+    $nomeb = $linha->nome;
+    $userb = $linha->email;
+    $senhab = $linha->senha;
+    $tipob = $linha->tipo_usuario;
 }
 
-if($user == $userb AND $senha == $senhab AND $tipob == "u"){// verifica se usuário, senha estão corretos e se ele é administrador
+if ($user == $userb AND $senha == $senhab AND $tipob == "u") {// verifica se usuário, senha estão corretos e se ele é administrador
+    if (count($consulta) > 0) {
+        $_SESSION['email'] = $user;
+        $_SESSION['nome'] = strtoupper($nomeb);
+        $_SESSION['tipo'] = 'user';
 
-	if (count($consulta) > 0) {
-		$_SESSION['email'] = $user;
-		$_SESSION['nome'] = strtoupper($nomeb);
-		$_SESSION['tipo'] = 'user';
+        setcookie('email', $user, time() + 3600);
 
-		setcookie('email', $user, time()+3600);
+        $user_conf = $conexao->prepare("UPDATE usuario SET usuario_ativo = true WHERE id_usuario = :id_usuario");
+        $user_conf->bindValue(":id_usuario", $idb, PDO::PARAM_INT);
+        $user_conf->execute();
 
-		header("location:index.php");
-
-	}else{
-		 header("location:index.php");
-	}
-}else{
- 	header("location:erro_login.php");
+        header("location:index.php");
+    }
+    else {
+        header("location:index.php");
+    }
 }
-?>
+else {
+    ?>
+    <script>
+        var confirma = confirm("Erro ao logar. Verifique se email e senha estão corretos.");
+        
+        if (confirma) {
+            location.href="user_login.php";
+        }
+        else {
+            location.href="index.php";  
+        }
+    </script>
+    <?php
+}
