6f49c9752fc79a0004af29f07b7a5d4365548eed
Lucas Marques <lukkeemarxs@gmail.com>
2015-11-24 17:37:28 -0200

    Adicionado formulário de cadastro de sinalização

---
+++ m/sinal_form_cadastro_part1.php
@@ -0,0 +1,23 @@
+<?php include("template/header.php");?>
+
+	<div id="formWindow">
+		<form action="sinal_form_cadastro_part2.php" class="formSinal" name="formSinal" method="POST">
+			<center>
+				<h1>Cadastrar sinalização</h1><br>
+			</center>
+
+			<div id='formSinal'>
+				<label style="font-size:2em;">Titulo:</label>   <input type="text" name="titulo" class="formSinalInput" maxlength="45" minlength="3" required><br>
+				<label style="font-size:2em;">Categoria:</label><select name="categoria" class="formSinalInput selectSinal">
+				<option value="OB" selected>Obras</option>
+				<option value="IT">Interditado</option>
+				<option value="AC">Acidentado</option>
+				<option value="OT">Outros</option>
+				</select><br>
+				<textarea placeholder="Descrição aqui..."class="formSinalInput"name="descricao"></textarea><br>
+				<input type="submit" value="Adicionar Ponto" style="margin-right: -20px;" class="button botaoSubmitSinal">
+			</div>
+		</form>		
+	</div>
+</div>	
+<?php include("template/footer.php");?>
---
+++ m/sinal_form_cadastro_part2.php
@@ -0,0 +1,81 @@
+<?php
+require_once("template/header.php");
+?>
+
+			<div id="mapaSinal"></div>
+			<form action="sinal_confirmaCad.php"  name="formSinal" method="POST" style="position:fixed; bottom:5%; right:5%; z-index:100;">
+				<?php
+					$titulo = $_POST["titulo"];
+					$categoria = $_POST["categoria"];
+					$descricao = $_POST["descricao"];
+				?>
+				<input type="hidden" name="titulo" class="formSinalInput" maxlength="45" minlength="3" required value="<?php echo $titulo; ?>"><br>
+				<input type="hidden" name="categoria"  id="tipo" value="<?php echo $categoria; ?>">
+				<input type="hidden" style="display:none;" name="descricao" value="<?php echo $descricao; ?>">
+				<input type="hidden" name="latitude"   id="lat" value="-23.6255903">
+				<input type="hidden" name="longitude"   id="lng" value="-45.4241453"><br>
+				<input type="submit" value="Salvar" class="button botaoSubmitSinal" onclick="return validar();">
+			</form>
+
+	<script>
+		var iconObras = 'imagens/sinal_obras.png';
+		var iconInterditado = 'imagens/sinal_inderditado.png';
+		var iconAcidentado = 'imagens/sinal_acidentado.png';
+		var iconOutros = 'imagens/sinal_outros.png';
+		var marker = '';
+		var map = '';
+		var mlat = document.getElementById("lat").value;
+		var mlgn = document.getElementById("lng").value;
+
+		function initMap() {
+		   map = new google.maps.Map(document.getElementById('mapaSinal'), {
+			zoom: 15,
+			center: {lat: parseFloat(mlat), lng: parseFloat(mlgn)},
+			disableDefaultUI: true
+		  });
+		  
+		var categoria = document.getElementById("tipo").value;
+			if(categoria == 'OB'){
+				addMarker(iconObras);
+			}
+			if(categoria == 'IT'){
+				addMarker(iconInterditado);
+			}
+			if(categoria == 'AC'){
+				addMarker(iconAcidentado);
+			}
+			else if(categoria == 'OT'){
+				addMarker(iconOutros);
+			}
+			
+			google.maps.event.addListener(marker, "dragend", function(event){
+			 document.getElementById("lat").value = event.latLng.lat();
+			 document.getElementById("lng").value = event.latLng.lng();
+			});
+
+
+		}
+		
+		
+			
+			
+		
+		function addMarker(myicon){
+			 var mlat = document.getElementById("lat").value;
+			 var mlgn = document.getElementById("lng").value;
+			   marker = new google.maps.Marker({
+				  map: map,
+				  draggable: true,
+				  animation: google.maps.Animation.DROP,
+				  icon: myicon,
+				  position: {lat: parseFloat(mlat), lng: parseFloat(mlgn)}
+			  });
+			google.maps.event.addListener(marker, "dragend", function(event){
+			 document.getElementById("lat").value = event.latLng.lat();
+			 document.getElementById("lng").value = event.latLng.lng();
+			});
+		}
+
+		google.maps.event.addDomListener(window, 'load', initMap);
+	</script>
+<?php require_once("template/footer.php");?>
