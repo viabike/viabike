9fb921cc65284d8a544223f9b864d190658624ed
Lucas Perrotta Barbosa <perrotta.lucas@gmail.com>
2015-11-24 16:44:14 -0200

    Separado a alteração de senha

--- m/user_senha.php
+++ m/user_senha.php
@@ -8,6 +8,7 @@ echo '<center><h1>Alterar senha</h1></center>';
 ?>
 
 <form action="user_altera_senha.php" method="POST">
+  <span style="font-size: 2em">Senha atual: </span><input class="form" type="password" name="atual_senha"><br>
   <span style="font-size: 2em">Nova senha: </span><input class="form" type="password" name="nova_senha"><br>
   <span style="font-size: 2em">Confirma sua senha: </span><input class="form" type="password" name="conf_senha"><br>
   <input class="button" type="submit" value="Alterar" style="float: right">
