ccdf0874e12db95cd721ad11003f0a6666f0a0ff
itallo ferreira <itallorsf@gmail.com>
2015-11-29 13:23:34 -0200

    Adicionado verificação se campo 'Confirmar senha' confere com campo 'senha'

--- user_formulario.php
+++ user_formulario.php
@@ -4,43 +4,58 @@ require_once("template/header.php");
 <link rel="stylesheet" type="text/css" href="admin/css/style.css">
 
 <script>
-$(document).ready(function () {
-	$("#campoemail").css("border", "1px solid #bdc3c7");
+    $(document).ready(function() {
+        $("#campoemail").css("border", "1px solid #bdc3c7");
+        $("#campoSenha").css("border", "1px solid #bdc3c7");
+        $("#campoConfSenha").css("border", "1px solid #bdc3c7");
 
-	$("#campoemail").keyup(function(){
-		var email = $(this).val();
-			$.ajax({
-			type: "POST",
-			url: "user_formulario_verifica.php",
-			data: "email=" + email,
-			success: function(resultado){
-				var retorno = resultado;
-				getMessage(retorno);
-			},
-			error: function(){
-				alert("Erro ao verificar os detalhes do usuario!");
-			}
-		 });
-  });
-});
+        $("#campoConfSenha").keyup(function() {
+            var senha = $("#campoSenha").val();
+            var confSenha = $(this).val();
 
-function getMessage(type) {
-	if (type  == 1) {
-		$("#campoemail").css("border", "1px solid #f00");
-	}
-	else {
-		$("#campoemail").css("border", "1px solid #40bd68");
-	}
-}
- </script>
+            if (confSenha === senha) {
+                $("#campoConfSenha").css("border", "1px solid #40bd68");
+            }
+            else {
+                $("#campoConfSenha").css("border", "1px solid #f00");
+            }
+        });
+
+
+        $("#campoemail").keyup(function() {
+            var email = $(this).val();
+            $.ajax({
+                type: "POST",
+                url: "user_formulario_verifica.php",
+                data: "email=" + email,
+                success: function(resultado) {
+                    var retorno = resultado;
+                    getMessage(retorno);
+                },
+                error: function() {
+                    alert("Erro ao verificar os detalhes do usuario!");
+                }
+            });
+        });
+    });
+
+    function getMessage(type) {
+        if (type == 1) {
+            $("#campoemail").css("border", "1px solid #f00");
+        }
+        else {
+            $("#campoemail").css("border", "1px solid #40bd68");
+        }
+    }
+</script>
 
 <div id='form_user'>
     <form action="user_confirmaCad.php" class="form_user1" method="POST" style="border-right: 1px solid #bdc3c7;">
         <h1>Cadastre-se</h1><br>
         Nome Completo:<input type="text" name="nome" class="form" placeholder="Ex: Exemplo de Nome" required><br>
         E-mail:<input type="email" name="email" id="campoemail" class="form" placeholder="Ex: ex@exemplo.com" required><br>
-        Senha:<input type="password" name="senha" class="form" required><br>
-        Confirme sua senha:<input type="password" name="senha_confirma" class="form" required><br>
+        Senha:<input type="password" name="senha" id="campoSenha" class="form" required><br>
+        Confirme sua senha:<input type="password" name="senha_confirma" id="campoConfSenha" class="form" required><br>
         <input type="submit" value="Cadastrar" class="button">
     </form>
 
