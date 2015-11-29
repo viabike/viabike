d92f7d40aed0a42018937576209b0511d6026996
itallo ferreira <itallorsf@gmail.com>
2015-11-29 13:12:43 -0200

    Adicionado verificação se email já está sendo utilizado

--- user_formulario.php
+++ user_formulario.php
@@ -1,58 +1,55 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>ViaBike.me</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="admin/css/style.css">
-	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-	<script src="https://maps.googleapis.com/maps/api/js"></script>
-	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-</head>
-<body>
-	<div id="wrapper">
+<?php
+require_once("template/header.php");
+?>
+<link rel="stylesheet" type="text/css" href="admin/css/style.css">
 
-		<div id="header">
+<script>
+$(document).ready(function () {
+	$("#campoemail").css("border", "1px solid #bdc3c7");
 
-			<a href="index.php"><img src="imagens/viabike2.png" alt="ViaBike.me" class="logo"></a>
+	$("#campoemail").keyup(function(){
+		var email = $(this).val();
+			$.ajax({
+			type: "POST",
+			url: "user_formulario_verifica.php",
+			data: "email=" + email,
+			success: function(resultado){
+				var retorno = resultado;
+				getMessage(retorno);
+			},
+			error: function(){
+				alert("Erro ao verificar os detalhes do usuario!");
+			}
+		 });
+  });
+});
 
-			<div id="nav-header">
-				<ul>
-					<li><a href="equipe.php">EQUIPE</a></li>
-					<li><a href="sobre.php">SOBRE</a></li>
-					<li><a href="index.php">HOME</a></li>
-				</ul>
-			</div>
-		</div>
+function getMessage(type) {
+	if (type  == 1) {
+		$("#campoemail").css("border", "1px solid #f00");
+	}
+	else {
+		$("#campoemail").css("border", "1px solid #40bd68");
+	}
+}
+ </script>
 
-		<div id="container">
-			<div id="content">
+<div id='form_user'>
+    <form action="user_confirmaCad.php" class="form_user1" method="POST" style="border-right: 1px solid #bdc3c7;">
+        <h1>Cadastre-se</h1><br>
+        Nome Completo:<input type="text" name="nome" class="form" placeholder="Ex: Exemplo de Nome" required><br>
+        E-mail:<input type="email" name="email" id="campoemail" class="form" placeholder="Ex: ex@exemplo.com" required><br>
+        Senha:<input type="password" name="senha" class="form" required><br>
+        Confirme sua senha:<input type="password" name="senha_confirma" class="form" required><br>
+        <input type="submit" value="Cadastrar" class="button">
+    </form>
 
-				<CENTER>
-				<div id='form_user'>
-					<form action="user_confirmaCad.php" class="form_user1" method="POST" style="border-right: 1px solid #bdc3c7;">
-						<h1>Cadastre-se</h1><br>
-						Nome Completo:<input type="text" name="nome" class="form" placeholder="Ex: Exemplo de Nome" required><br>
-						E-mail:<input type="email" name="email" class="form" placeholder="Ex: ex@exemplo.com" required><br>
-						Senha:<input type="password" name="senha" class="form" required><br>
-						Confirme sua senha:<input type="password" name="senha_confirma" class="form" required><br>
-						<input type="submit" value="Cadastrar" class="button">
-					</form>
-
-					<form action="confirma_login.php" class="form_user2" method="POST">
-						<h1>Entrar</h1><br>
-						E-mail:<input type="email" name="email" class="form" placeholder="Ex: ex@exemplo.com" required><br>
-						Senha:<input type="password" name="senha" class="form" required><br>
-						<input type="submit" value="Entrar" class="button">
-					</form>
-				</div>
-				</CENTER>
-
-			</div>
-		</div>
-		<div id="footer">
-			<center><p>&copy ViaBike.me - 2015 <br>Site em desenvolvimento</p></center>
-		</div>
-	</div>
-</body>
-</html>
+    <form action="confirma_login.php" class="form_user2" method="POST">
+        <h1>Entrar</h1><br>
+        E-mail:<input type="email" name="email" class="form" placeholder="Ex: ex@exemplo.com" required><br>
+        Senha:<input type="password" name="senha" class="form" required><br>
+        <input type="submit" value="Entrar" class="button">
+    </form>
+</div>
+<?php
+require_once("template/footer.php");
