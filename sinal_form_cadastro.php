17b17347abed6a26e6389ec9f53f16e5e46176c8
BrianSNS <brian.caragua2013@gmail.com>
2015-11-30 16:18:53 -0200

    Adicionado KML ao mapa do formulário de cadastro de sinalização

--- sinal_form_cadastro.php
+++ sinal_form_cadastro.php
@@ -20,7 +20,7 @@ require_once("template/header.php");
         <div id="mapaSinal"></div>
         <input  type="hidden" name="latitude" class="formSinalInput latLng" id="lat" value="-23.6255903">
         <input  type="hidden" name="longitude" class="formSinalInput latLng lng" id="lng" value="-45.4241453">
-        <input type="submit" value="Cadastrar" class="button sinalizacao botaoSubmitSinal"  onclick="return validar();">
+        <input type="submit" value="Cadastrar" class="button sinalizacao botaoSubmitSinal" onclick="return validar();">
     </div>
 </form>
 <script>
@@ -32,63 +32,68 @@ require_once("template/header.php");
     var map = '';
     var mlat = document.getElementById("lat").value;
     var mlgn = document.getElementById("lng").value;
+	
+		function initMap() {
+		   map = new google.maps.Map(document.getElementById('mapaSinal'), {
+			zoom: 15,
+			center: {lat: -23.6255903, lng: -45.4241453}
+		  });
 
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('mapaSinal'), {
-            zoom: 15,
-            center: {lat: -23.6255903, lng: -45.4241453}
-        });
+			addMarker(iconOb);
 
-        addMarker(iconOb);
+			google.maps.event.addListener(marker, "dragend", function(event){
+			 document.getElementById("lat").value = event.latLng.lat();
+			 document.getElementById("lng").value = event.latLng.lng();
+			});
 
-        google.maps.event.addListener(marker, "dragend", function(event) {
-            document.getElementById("lat").value = event.latLng.lat();
-            document.getElementById("lng").value = event.latLng.lng();
-        });
+		loadKmlLayer('http://viabike.me/mapa/mapa-das-ciclovias-v2.kml', map);
+		}
+		
 
-    }
+		$("#categoria_sinal").change(function() {
+			var categoria_sinal = document.getElementById("categoria_sinal").value;
+			if(categoria_sinal == 'OB'){
+				marker.setMap(null);
+				addMarker(iconOb);
+			}
+			if(categoria_sinal == 'IT'){
+				marker.setMap(null);
+				addMarker(iconIt);
+			}
+			if(categoria_sinal == 'AC'){
+				marker.setMap(null);
+				addMarker(iconAc);
+			}
+			if(categoria_sinal == 'OT'){
+				marker.setMap(null);
+				addMarker(iconOt);
+			}
+		});
 
-    $("#categoria_sinal").change(function() {
+		function addMarker(myicon){
+			 var mlat = document.getElementById("lat").value;
+			 var mlgn = document.getElementById("lng").value;
+			   marker = new google.maps.Marker({
+				  map: map,
+				  draggable: true,
+				  animation: google.maps.Animation.DROP,
+				  icon: myicon,
+				  position: {lat: parseFloat(mlat), lng: parseFloat(mlgn)}
+			  });
+			google.maps.event.addListener(marker, "dragend", function(event){
+			 document.getElementById("lat").value = event.latLng.lat();
+			 document.getElementById("lng").value = event.latLng.lng();
+			});
+		}
+		
+		function loadKmlLayer(src, map) {
+                var kmlLayer = new google.maps.KmlLayer(src, {
+                    suppressInfoWindows: true,
+                    preserveViewport: true,
+                    map: map
+                });
+            }
 
-        var categoria_sinal = document.getElementById("categoria_sinal").value;
-
-        if (categoria_sinal == 'OB') {
-            marker.setMap(null);
-            addMarker(iconOb);
-        }
-        if (categoria_sinal == 'IT') {
-            marker.setMap(null);
-            addMarker(iconIt);
-        }
-        if (categoria_sinal == 'AC') {
-            marker.setMap(null);
-            addMarker(iconAc);
-        }
-        if (categoria_sinal == 'OT') {
-            marker.setMap(null);
-            addMarker(iconOt);
-        }
-
-    });
-
-
-    function addMarker(myicon) {
-        var mlat = document.getElementById("lat").value;
-        var mlgn = document.getElementById("lng").value;
-        marker = new google.maps.Marker({
-            map: map,
-            draggable: true,
-            animation: google.maps.Animation.DROP,
-            icon: myicon,
-            position: {lat: parseFloat(mlat), lng: parseFloat(mlgn)}
-        });
-        google.maps.event.addListener(marker, "dragend", function(event) {
-            document.getElementById("lat").value = event.latLng.lat();
-            document.getElementById("lng").value = event.latLng.lng();
-        });
-    }
-
-    google.maps.event.addDomListener(window, 'load', initMap);
-</script>
-<?php
-require_once("template/footer.php");
+		google.maps.event.addDomListener(window, 'load', initMap);
+	</script>
+<?php include("template/footer.php");?>
