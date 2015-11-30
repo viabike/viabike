0be5752740342e8881c9262c91b0de601480d1b6
itallo ferreira <itallorsf@gmail.com>
2015-11-30 12:08:19 -0200

    Adicionado favicon

--- m/index.php
+++ m/index.php
@@ -23,6 +23,8 @@ $linhaSinal = $buscaSinal->fetchAll(PDO::FETCH_OBJ);
         <title>Viabike.me</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="imagens/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
         <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
