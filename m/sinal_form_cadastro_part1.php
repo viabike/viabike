7f5137b82615dfa183b9565f80b68383d2bd811d
haryelluyde <haryelllc@gmail.com>
2015-11-30 16:43:49 -0200

    Formatação do formulário da página de sinal_form_cadastro_part1

--- m/sinal_form_cadastro_part1.php
+++ m/sinal_form_cadastro_part1.php
@@ -1,23 +1,25 @@
-<?php include("template/header.php");?>
+<?php require_once("template/header.php");?>
 
+	<center>
 	<div id="formWindow">
 		<form action="sinal_form_cadastro_part2.php" class="formSinal" name="formSinal" method="POST">
 			<center>
 				<h1>Cadastrar sinalização</h1><br>
 			</center>
 
-			<div id='formSinal'>
-				<label style="font-size:2em;">Titulo:</label>   <input type="text" name="titulo" class="formSinalInput" maxlength="45" minlength="3" required><br>
-				<label style="font-size:2em;">Categoria:</label><select name="categoria" class="formSinalInput selectSinal">
+			<div class="form_user2">
+				<span style="font-size:2em;">Titulo:</span><input type="text" name="titulo" style="width:calc(90% - 2px);" class="form" maxlength="45" minlength="3" required><br>
+				<span style="font-size:2em;">Categoria:</span>
+				<select name="categoria" class="form" style="width:100%">
 				<option value="OB" selected>Obras</option>
 				<option value="IT">Interditado</option>
 				<option value="AC">Acidentado</option>
 				<option value="OT">Outros</option>
 				</select><br>
-				<textarea placeholder="Descrição aqui..."class="formSinalInput"name="descricao"></textarea><br>
-                <input type="submit" value="Localização >>" style="margin-right: -20px;" class="button botaoSubmitSinal">
+				<span style="font-size:2em;">Descrição:</span><textarea style="padding:5px; width:calc(100% - 12px);" class="form" name="descricao"></textarea><br>
+                <input type="submit" value="Prosseguir" style="float:right;" class="button botaoSubmitSinal">
 			</div>
-		</form>		
+		</form>
 	</div>
-</div>	
-<?php include("template/footer.php");?>
+	<center>
+<?php require_once("template/footer.php");?>
