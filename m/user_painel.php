15a027faaa8988bdfdd3818dd84876a50be4ba19
itallo ferreira <itallorsf@gmail.com>
2015-12-05 17:05:35 -0200

    Modificado como são impressos os dados vindos do banco

--- m/user_painel.php
+++ m/user_painel.php
@@ -2,9 +2,7 @@
 require_once("../conexao/conexao.php");
 require_once("template/header.php");
 
-
 echo '<center><h1>Editar Perfil</h1></center>';
-// <a href="user_senha.php"><button class="button" style="float: left;">Alterar senha</button></a></h1><br>';
 
 $conexao = conectar(); //Conexao com o banco de dados viabike_db
 
@@ -16,14 +14,20 @@ $user = $user_buscador->fetchAll(PDO::FETCH_OBJ);
 
 
 foreach ($user as $usuario):
-    ?>
+    $id_usuario = $usuario->id_usuario;
+    $nome = $usuario->nome;
+    $email = $usuario->email;
+    $foto = $usuario->foto;
+endforeach;
+
+?>
     <form action="user_altera.php" method="POST" enctype="multipart/form-data" style="float:left;">
-        <input type="hidden" name="id_usuario" value="<?= $usuario->id_usuario ?>">
-        <span style="font-size: 2em">Nome:</span><input type="text" name="nome" class="form" value="<?= $usuario->nome ?>" placeholder="Ex: Exemplo de Nome" required><br>
-        <span style="font-size: 2em">E-mail:</span><input type="email" name="email" class="form" value="<?= $usuario->email ?>" placeholder="Ex: ex@exemplo.com" required><br>
-        <img src="../imagens/users/<?= $usuario->foto ?>" width="100%">
+        <input type="hidden" name="id_usuario" value="<?php echo $id_usuario ?>">
+        <span style="font-size: 2em">Nome:</span><input type="text" name="nome" class="form" value="<?php echo $nome ?>" placeholder="Ex: Exemplo de Nome" required><br>
+        <span style="font-size: 2em">E-mail:</span><input type="email" name="email" class="form" value="<?php echo $email ?>" placeholder="Ex: ex@exemplo.com" required><br>
+        <img src="../imagens/users/<?php echo $foto ?>" width="100%">
         <span style="font-size: 2em">Foto:</span><input type="file" name="foto" class="form"><br>
-        <input type="hidden" name="foto_velha" value="<?= $usuario->foto ?>">
+        <input type="hidden" name="foto_velha" value="<?php echo $foto ?>">
 
         <hr style="border:1px; padding:2px; margin-bottom:10px; border-radius:5px; background-color:rgba(204,204,204, 0.25);">
 
@@ -40,11 +44,10 @@ foreach ($user as $usuario):
         if (confirma1) {
            var confirma2 = confirm("Tem certeza? \nAs sinalizações cadastradas por você não apareceram mais no mapa.");
            if (confirma2) {
-               location.href="user_desativar.php?id_usuario=<?= $usuario->id_usuario ?>";
+               location.href="user_desativar.php?id_usuario=<?php echo $id_usuario ?>";
            }
         }
     }
     </script>
 <?php
-endforeach;
 require_once("template/footer.php");
