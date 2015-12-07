d2d6cbada66649d5464626452d100875355a1453
Haryel Luyde <haryelllc@gmail.com>
2015-12-07 17:49:44 -0200

    Adicionado script que fecha mensagem ao clicar no botão

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
-$(document).ready(function(){
-	$("#menu").hide();
-	var aux = 0;
-	
-	$("#nav" ).click(function() {
-		if (aux == 0) {
-			aux = 1;
-			$("#menu" ).show();
-		}
-		else {
-			$("#menu").hide();
-			aux = 0;
-		}
-	});	
+$(document).ready(function() {
+	// script para fechar mensagem de erro quando clicar no X
+	// página user_formulario.php
+	$("#upClose").click(function() {
+		$("#upMsg").hide();
+	});
+
+	$("#ufNClose").click(function() {
+		$("#ufNMsg").hide();
+	});
+
+	$("#ufEClose").click(function() {
+		$("#ufEMsg").hide();
+	});
+
+	$("#ufSClose").click(function() {
+		$("#ufSMsg").hide();
+	});
+
+	$("#ufEIClose").click(function() {
+		$("#ufEIMsg").hide();
+	});
+
+	$("#emailIndisponivelC").click(function() {
+		$("#emailIndisponivel").hide();
+	});
+
+	$("#senhasDiferentesC").click(function() {
+		$("#senhasDiferentes").hide();
+	});
+
+	$("#senhasIguaisC").click(function() {
+		$("#senhasIguais").hide();
+	});
+
 });
